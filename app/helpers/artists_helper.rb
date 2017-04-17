3324a2b3da38714a102cd7fa0e0223ff72ba2d68
github username pianistps <github email address pianistps@yahoo.com>
2017-04-17 13:26:40 -0500

    Done.

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -2,4 +2,18 @@ module ArtistsHelper
   def display_artist(song)
     song.artist.nil? ? link_to("Add Artist", edit_song_path(song)) : link_to(song.artist_name, artist_path(song.artist))
   end
+
+  def artist_select(artist, song)
+    if artist
+      artist.name
+    else
+      select_tag "song[artist_id]", options_from_collection_for_select(Artist.all, :id, :name)
+    end
+  end
 end
+
+# create a helper to display a drop-down list of artists if
+# someone edits a song directly via /songs/id/edit and to only display the artist's
+# name if they are editing through nested routing. Name the helper method artist_select.
+# Hint: You'll need to set a variable in the controller action to pass to the helper method as an argument
+# along with a song instance.
